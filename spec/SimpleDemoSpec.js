e9e4e2d1333de8b418e1772af3ea90d224e207e7
EmmaATester <emma_deakin@hotmail.com>
2015-05-14 12:15:05 +0100

    Examples Jasmine spec files
    Included are example Jasmine spec files for testing the JavaScript file
    src/SimpleDemo.js file which is used in the supplied Demo.html file.
    The example Jasmine files show a simple spec, use of some matchers and
    use of not with matchers.  there is an example custom matcher and an
    example spy.

---
+++ spec/SimpleDemoSpec.js
@@ -0,0 +1,15 @@
+
+//this is the suite
+describe("Simple demo", function() {
+ 	
+ 
+ //this is the spec
+  it("tells you what the test does", function() {
+
+	//arange
+	//act
+	//assert
+	expect(true).toBeTruthy();
+ });
+
+});
